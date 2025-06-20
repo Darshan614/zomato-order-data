23260684203753bff85db0b57fb3715383044d58
Darshan614 <jaindarshan614@gmail.com>
2025-06-21 00:30:07 +0530

    common protocol

--- mssql_to_bq/scripts/sqlserver_to_bq_job.py
+++ mssql_to_bq/scripts/sqlserver_to_bq_job.py
@@ -130,7 +130,7 @@ def access_secrets(env, gcp_project_id, bq_dataset_name, bq_temp_gcs_bucket):
 def get_db_config(env, secrets=None):
     if secrets is None:
         secrets = access_secrets(env)
-    jdbc_url = f"jdbc:sqlserver://{secrets['host']}:1433;databaseName={secrets['database']};encrypt=true;trustServerCertificate=true"
+    jdbc_url = f"jdbc:sqlserver://{secrets['host']}:1433;databaseName={secrets['database']};encrypt=true;trustServerCertificate=true;sslProtocol=TLSv1.2"
     # jdbc_url = f"jdbc:sqlserver://{secrets['host']}:1433;databaseName={secrets['database']};encrypt=false;"
     connection_properties = {
         "user": secrets["user"],
